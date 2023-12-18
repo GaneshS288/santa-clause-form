a4a5b43074ac826530ba563392aa815a7023b621
GaneshS288 <swamiganesh288@gmail.com>
2023-12-18 09:19:15 +0530

    add error messages

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let password = document.querySelector("#password");
-let pwd_repeat = document.querySelector(".error");
+let pwd_repeat = document.querySelector(".error"); 
+let pwdRepeatError = document.querySelector(".errorMessage");
 
 pwd_repeat.addEventListener("input", (e) => {
     password = document.querySelector("#password");
     if (e.target.value === password.value && e.target.value !== "") {
         e.target.style.border = "2px solid green"
+        pwdRepeatError.textContent = ""
     }
 
     else if (e.target.value === "") {
         e.target.style.border = "2px solid black"
+        pwdRepeatError.textContent = ""
     }
 
-    else { e.target.style.border = "2px solid orangered"}
+    else { 
+        e.target.style.border = "2px solid orangered"
+        pwdRepeatError.textContent = "*passwords do not match"
+    }
 })
